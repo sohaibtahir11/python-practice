fc279852584578e019622f2d23ab8f30a376641e
Sohaib <sohaibtahir9898@gmail.com>
2026-07-03 18:56:00 +0500

    day 2 - lists and conditionals challenge

--- 1.py
+++ 1.py
@@ -13,17 +13,58 @@
 #     print("Temperature too low — see a doctor")
 
 
-fruits = ["Mango","Apple", "Banana", "Watermelon", "Pomegrant"]
-fruits5=[]
-if(len(fruits[0])>5):
-    fruits5.append(fruits[0])
-if(len(fruits[1])>5):
-   fruits5.append(fruits[1])
-if(len(fruits[2])>5):
-   fruits5.append(fruits[2])
-if(len(fruits[3])>5):
-    fruits5.append(fruits[3])
-if(len(fruits[4])>5):
-    fruits5.append(fruits[4])
+# fruits = ["Mango","Apple", "Banana", "Watermelon", "Pomegrant"]
+# fruits5=[]
+# if(len(fruits[0])>5):
+#     fruits5.append(fruits[0])
+# if(len(fruits[1])>5):
+#    fruits5.append(fruits[1])
+# if(len(fruits[2])>5):
+#    fruits5.append(fruits[2])
+# if(len(fruits[3])>5):
+#     fruits5.append(fruits[3])
+# if(len(fruits[4])>5):
+#     fruits5.append(fruits[4])
     
-print(fruits5)
+# print(fruits5)
+
+
+
+students = ["Ali", "Sara", "Usman", "Hina", "Bilal"]
+marks = [45, 72, 38, 85, 60]
+
+if(marks[0]>=50):
+    print(students[0], "Pass")
+else:
+    print(students[0], "Fail")
+if(marks[1]>=50):
+    print(students[1], "Pass")
+else:
+    print(students[1], "Fail")
+if(marks[2]>=50):
+    print(students[2], "Pass")
+else:
+    print(students[2], "Fail")
+if(marks[3]>=50):
+    print(students[3], "Pass")
+else:
+    print(students[3], "Fail")
+if(marks[4]>=50):
+    print(students[4], "Pass")
+else:
+    print(students[4], "Fail")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
