2463667f3d0671cbde9e04028a8770bd5c8a7f3f
Sohaib <sohaibtahir9898@gmail.com>
2026-07-08 02:03:04 +0500

    Practice Questions

--- 1.py
+++ 1.py
@@ -30,41 +30,74 @@
 
 
 
-students = ["Ali", "Sara", "Usman", "Hina", "Bilal"]
-marks = [45, 72, 38, 85, 60]
+# students = ["Ali", "Sara", "Usman", "Hina", "Bilal"]
+# marks = [45, 72, 38, 85, 60]
 
-if(marks[0]>=50):
-    print(students[0], "Pass")
-else:
-    print(students[0], "Fail")
-if(marks[1]>=50):
-    print(students[1], "Pass")
-else:
-    print(students[1], "Fail")
-if(marks[2]>=50):
-    print(students[2], "Pass")
-else:
-    print(students[2], "Fail")
-if(marks[3]>=50):
-    print(students[3], "Pass")
-else:
-    print(students[3], "Fail")
-if(marks[4]>=50):
-    print(students[4], "Pass")
-else:
-    print(students[4], "Fail")
+# if(marks[0]>=50):
+#     print(students[0], "Pass")
+# else:
+#     print(students[0], "Fail")
 
+# if(marks[1]>=50):
+#     print(students[1], "Pass")
+# else:
+#     print(students[1], "Fail")
 
+# if(marks[2]>=50):
+#     print(students[2], "Pass")
+# else:
+#     print(students[2], "Fail")
 
-
-
-
+# if(marks[3]>=50):
+#     print(students[3], "Pass")
+# else:
+#     print(students[3], "Fail")
+    
+# if(marks[4]>=50):
+#     print(students[4], "Pass")
+# else:
+#     print(students[4], "Fail")
 
 
+#Question 1
+# radius = float(input("Write the radius of the circle: "))
+# pi = 3.14
+
+# area = pi*radius*radius
+# circumference = 2*pi*radius
+
+# print("Area of the circle is", area)
+# print("Circumference of the circle is", circumference)
 
 
+#Quesiton2
+# a = 10
+# b = 20
+
+# a,b =b,a
+# print (a,b)
 
 
+ # Question3
+# number = int(input("Write any number: "))
+
+# if(number%2==0):
+#     Status="Even"
+# else:
+#     Status="Odd"
+# print("Your Number is:",Status)
 
 
+# Question 4
+Age=int(input("Write Your Age: "))
 
+if(0<=Age<=12):
+    Status="Child"
+elif(13<=Age<=19):
+    Status="Teenager"
+elif(20<=Age<=59):
+    Status="Adult"
+elif(59<Age):
+    Status="Senior"
+
+print("Your Status is:", Status)
