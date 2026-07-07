f0c6535c18230e4889b9dcfbe46dcb7fa85f4d37
Sohaib <sohaibtahir9898@gmail.com>
2026-07-08 02:10:55 +0500

    Solved practice question

--- 1.py
+++ 1.py
@@ -70,7 +70,7 @@
 # print("Circumference of the circle is", circumference)
 
 
-#Quesiton2
+# Quesiton2
 # a = 10
 # b = 20
 
@@ -78,7 +78,7 @@
 # print (a,b)
 
 
- # Question3
+#  Question3
 # number = int(input("Write any number: "))
 
 # if(number%2==0):
@@ -89,15 +89,25 @@
 
 
 # Question 4
-Age=int(input("Write Your Age: "))
+# Age=int(input("Write Your Age: "))
 
-if(0<=Age<=12):
-    Status="Child"
-elif(13<=Age<=19):
-    Status="Teenager"
-elif(20<=Age<=59):
-    Status="Adult"
-elif(59<Age):
-    Status="Senior"
+# if(0<=Age<=12):
+#     Status="Child"
+# elif(13<=Age<=19):
+#     Status="Teenager"
+# elif(20<=Age<=59):
+#     Status="Adult"
+# elif(59<Age):
+#     Status="Senior"
 
-print("Your Status is:", Status)
+# print("Your Status is:", Status)
+num1=int(input("write the first number: "))
+num2=int(input("write the second number: "))
+num3=int(input("write the Third number: "))
+
+if(num1>num2 and num1>num3):
+    print(num1,"is the greatest number!")
+elif(num2>num1 and num2>num3):
+    print(num2,"is the Greatest number!")
+else:
+    print(num3,"is the greatest number!")
